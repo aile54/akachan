89cd2e700c83e74ec6cd7546337a724a2bacc075
anhpq <anhpq60228@gmail.com>
2016-03-29 17:12:45 +0700

    Fix ko chạy dc counter
    Fixed

--- Controllers/countervisitors.php
+++ Controllers/countervisitors.php
@@ -1,4 +1,36 @@
 <? include('counter.php')?>
+<style>	
+	#counter_visitors .control-group {
+		margin: 0;
+	}
+	
+	#counter_visitors .control-label {
+		text-align: left;
+		font-weight: bolder;
+		width: auto;
+		padding: 7px 0 0 26px;
+		margin: 0;
+	}
+	
+	#counter_visitors .controls {
+		text-align: right;
+		margin-top: 8px;
+		margin-left: 190px;
+		padding-right: 25px;
+	}
+</style>
+<script>
+ $(document).ready(function(){
+	 
+	 debugger;
+	$("#counter_visitors").mouseout(function () {
+		$("#counter_visitors").animate({ left: "-271px" }, { queue: false, duration: 500 })
+	});
+	$("#counter_visitors").mouseover(function () {
+		$("#counter_visitors").animate({ left: "0" }, { queue: false, duration: 500 })
+	});
+ });
+</script>
 <div style="position: fixed; left: -271px; bottom: 50px; z-index: 1000; cursor: pointer; background-image: url(../Templates/Content/images/icon/paper.png); background-size: 100% 100%" id="counter_visitors">
 	<div style="" class="form-horizontal">
         <div class="control-group">
@@ -28,33 +60,3 @@
         </div>
 	</div>
 </div>
-<style>	
-	#counter_visitors .control-group {
-		margin: 0;
-	}
-	
-	#counter_visitors .control-label {
-		text-align: left;
-		font-weight: bolder;
-		width: auto;
-		padding: 7px 0 0 26px;
-		margin: 0;
-	}
-	
-	#counter_visitors .controls {
-		text-align: right;
-		margin-top: 8px;
-		margin-left: 190px;
-		padding-right: 25px;
-	}
-</style>
-<script>
- $(document).ready(function(){	
-	$("#counter_visitors").mouseout(function () {
-		$("#counter_visitors").animate({ left: "-271px" }, { queue: false, duration: 500 })
-	});
-	$("#counter_visitors").mouseover(function () {
-		$("#counter_visitors").animate({ left: "0" }, { queue: false, duration: 500 })
-	});
- });
-</script>
