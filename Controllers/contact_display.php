8acf2d35e8cd2e07fb354ee4f95a7f5d10a1aba8
Ai Le <lenguyenai@icloud.com>
2016-06-23 21:24:27 +0700

    20160623
    sua contact_display khi dung tai khoan tren web

--- Controllers/contact_display.php
+++ Controllers/contact_display.php
@@ -20,14 +20,14 @@
     	if($result_setting != null && count($result_setting) > 0)
 			{
 		?>
-                <div class="mail" style="float: right; margin-left: 5px; margin-right: 5px; margin-top: 4px;">
+                <div class="mail" style="float: right; margin-top: 4px;">
                     <img src="../Templates/Content/images/mail.ico" style="margin-right: 5px;
                         float: left; width:20px" />
                     <strong style="margin-right: 5px; float: right; color: black">
                         <?php echo $result_setting[0]["email"]?>
                     </strong>
                 </div>
-                <div class="fb" style="float: right; margin-left: 5px; margin-right: 5px; margin-top: 4px;">
+                <div class="fb" style="float: right; margin-right: 5px; margin-top: 4px;">
                     <img src="../Templates/Content/images/icon/facebook.png" style="margin-right: 5px;
                         float: left; width:20px" />
                         
